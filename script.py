7bed78443caf7f5fe5800f4c94b76a8b65f79f0f
Pedro Markun <pedro@markun.com.br>
2017-05-31 12:52:08 -0300

    script que pega pautas

---
+++ script.py
@@ -0,0 +1,22 @@
+from lxml.html import parse
+from urllib.request import urlopen, quote
+import os
+
+def download_and_save(url):
+	filename = url.split('/')[-1]
+	if not os.path.isfile(filename):
+		url = '/'.join(url.split('/')[:-1]) + '/' + quote(filename)
+		if url:
+			pauta = urlopen(url)
+			with open(filename, 'b+w') as f:
+				f.write(pauta.read())
+
+pauta_url = "http://www.camara.sp.gov.br/atividade-legislativa/sessao-plenaria/pauta-das-sessoes/"
+
+soup = parse(pauta_url).getroot()
+
+for p in soup.xpath("//ul[@class='box-downloads-list']/li/a"):
+	try:
+		download_and_save(p.get('href'))
+	except:
+		print("Could not download " + p.get('href'))
